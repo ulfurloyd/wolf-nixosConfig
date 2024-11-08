3709c7e4ec7692a1351238d972aac5615a69475f
Eklavya Sood <eklavyasood@gmail.com>
2024-11-08 21:15:13 +0530

    polybar module

--- config/wm/polybar.nix
+++ config/wm/polybar.nix
@@ -10,12 +10,33 @@
         height = "18";
         radius = "7.0";
         "modules-center" = "bspwm";
+        "modules-left" = "date";
         wm-restack = "bspwm";
       };
 
       "module/bspwm" = {
         type = "internal/bspwm";
         label-focused = "%name%";
+      };
+
+      "module/date" = {
+        type = "internal/date";
+        
+        # Seconds to sleep between updates
+        # Default: 1.0
+        interval = "1.0";
+        
+        # See "https://en.cppreference.com/w/cpp/io/manip/put_time" for details on how to format the date string
+        # NOTE: if you want to use syntax tags here you need to use %%{...}
+        date = "%Y-%m-%d%";
+        
+        # Optional time format
+        time = "%H:%M";
+        
+        # if `date-alt` or `time-alt` is defined, clicking
+        # the module will toggle between formats
+        date-alt = "%A, %d %B %Y";
+        time-alt = "%H:%M:%S";
       };
     };
   };
