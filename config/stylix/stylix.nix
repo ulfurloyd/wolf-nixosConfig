fdb94ec815db78f6054cc332b1679b5ca417a666
Eklavya Sood <eklavyasood@gmail.com>
2024-11-09 21:38:53 +0530

    stylix home-manager configuration

--- config/stylix/stylix.nix
+++ config/stylix/stylix.nix
@@ -2,7 +2,31 @@
 {
   stylix = {
     enable = true;
+
     image = ../../wallpapers/bleak_cabin.png;
+
     polarity = "dark";
+
+    fonts = {
+      serif = {
+        package = pkgs.fira;
+        name = "Fira Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.fira-sans;
+        name = "Fira Sans Serif";
+      };
+
+      monospace = {
+        package = pkgs.fira-code;
+        name = "Fira Monospace";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 }
