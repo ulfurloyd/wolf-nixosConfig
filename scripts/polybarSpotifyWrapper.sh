9b1d232ff8e92c6fc12724751190dff3f7380322
Eklavya Sood <eklavyasood@gmail.com>
2024-11-10 22:20:28 +0530

    finally implemented polybar spotify module

--- scripts/polybarSpotifyWrapper.sh
+++ scripts/polybarSpotifyWrapper.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-nix develop /home/wolf/.dotfiles --override-input nixpkgs github:NixOS/nixpkgs --command python /home/wolf/.dotfiles/scripts/spotify_status.py
+nix-shell -p python311Packages.dbus-python --run "python /home/wolf/.dotfiles/scripts/spotify_status.py"
+
+# nix develop /home/wolf/.dotfiles --override-input nixpkgs github:NixOS/nixpkgs --command python /home/wolf/.dotfiles/scripts/spotify_status.py
