f8e86ab96c67c40a7e277803b29a00c80f00b0ed
eklavya <eklavyasood@gmail.com>
2024-11-29 19:15:42 +0530

    small keyd change

--- modules/common/keyd/default.nix
+++ modules/common/keyd/default.nix
@@ -6,7 +6,7 @@
         ids = [ "*" ];
         settings = {
           main = {
-            capslock = "overload(meta, esc)";
+            capslock = "esc";
             esc = "capslock";
           };
         };
