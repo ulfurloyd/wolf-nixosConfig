c2c781be1014b6d17c8195a9e2f27efc40a7ce74
eklavya <eklavyasood@gmail.com>
2024-12-02 18:42:57 +0530

    some more dap work, needs more tho

--- home/laptop/config/nvim/plugins/dap.nix
+++ home/laptop/config/nvim/plugins/dap.nix
@@ -1,5 +1,27 @@
 {
   programs.nixvim.plugins.dap = {
     enable = true;
+
+    configurations = {
+      c = [
+        {
+        name = "Launch";
+        type = "gdb";
+        request = "launch";
+        }
+        {
+        name = "Select and attach to process";
+        type = "gdb";
+        request = "attach";
+        }
+      ];
+    };
+
+    extensions = {
+      dap-ui.enable = true;
+      dap-virtual-text.enable = true;
+      dap-python.enable = true;
+      dap-go.enable = true;
+    };
   };
 }
