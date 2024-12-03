796342f829dae964748cac476009d7aaa1000a54
eklavya <eklavyasood@gmail.com>
2024-12-03 19:24:51 +0530

    minor nvim configs

--- home/laptop/config/nvim/plugins/which-key.nix
+++ home/laptop/config/nvim/plugins/which-key.nix
@@ -2,6 +2,20 @@
   programs.nixvim.plugins = {
     which-key = {
       enable = true;
+
+      settings = {
+        spec = [
+          {
+            __unkeyed-1 = "<leader>b";
+            group = "Buffers";
+            icon = "󰓩 ";
+          }
+          {
+            __unkeyed-1 = "<leader>f";
+            group = "File Options";
+          }
+        ];
+      };
     };
   };
 }
