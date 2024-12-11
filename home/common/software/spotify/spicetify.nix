5dc9b06f59205494d35e0b439edd30f5230ad8d8
eklavya <eklavyasood@gmail.com>
2024-12-11 11:24:32 +0530

    spicetify config

--- home/common/software/spotify/spicetify.nix
+++ home/common/software/spotify/spicetify.nix
@@ -7,14 +7,5 @@ in
     enable = true;
     spicetifyPackage = inputs.nixpkgs-unstable.legacyPackages."${pkgs.system}".spicetify-cli;
     enabledExtensions = with spicePkgs.extensions; [ ];
-    # theme = {
-    #   name = "base16-spotify";
-    #   src = pkgs.fetchFromGithub {
-    #     owner = "Misterio77";
-    #     repo = "base16-spotify";
-    #     rev = "";
-    #     hash = "";
-    #   };
-    # };
   };
 }
