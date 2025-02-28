ca7438831de49a523c228276ad5ccd235f945b70
eklavya <eklavyasood@gmail.com>
2025-02-28 18:46:01 +0530

    Nyx graphics changes

--- modules/nyx/nvidia.nix
+++ modules/nyx/nvidia.nix
@@ -1,15 +1,26 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
 
-    open = true;
+    open = false;
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.86.16";
+      sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+      # sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = lib.fakeSha256;
+    };
+
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
