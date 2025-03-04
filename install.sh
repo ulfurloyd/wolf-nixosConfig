aad6fcec49bbc9127d5e3b4c0f073a01b8f17046
eklavya <eklavyasood@gmail.com>
2025-03-04 19:57:39 +0530

    kinda functional, git has issues cloning the repo on a fresh install

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 SCRIPT_DIR=~/.dotfiles
 
 # Determine host type (Nyx or Lykaon)
-if [ $1 = "nyx" ]; then
+if [ "$1" == "nyx" ]; then
     HOST="nyx"
-elif [ $1 = "lykaon" ]; then
+elif [ "$1" == "lykaon" ]; then
     HOST="lykaon"
 else
     echo "Enter either 'nyx' (desktop) or 'lykaon' (laptop)"
@@ -18,6 +18,10 @@ fi
 nix-shell -p git --command "git clone https://github.com/eklavyasood/nixos-config $SCRIPT_DIR"
 
 # Generate hardware config for the target system
-sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/hosts/$HOST/hardware-configuration.nix
+sudo nixos-generate-config --show-hardware-config | sudo tee $SCRIPT_DIR/hosts/$HOST/hardware-configuration.nix
 
+# Build the system
+sudo nixos-rebuild switch --flake $SCRIPT_DIR#$HOST
 
+# Build the home-manager configuration
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#$HOST
