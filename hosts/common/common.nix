2c27e30550ad0fd06c087eb6a4bd6691c588374e
eklavya <eklavyasood@gmail.com>
2025-03-13 00:07:28 +0530

    added japanese and icelandic fcitx locales, and fcitx configuration

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -61,10 +61,19 @@ in
   };
 
   i18n = {
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_IN/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+    ];
+
     inputMethod = {
       enable = true;
-      type = "ibus";
-      ibus.engines = with pkgs.ibus-engines; [ mozc ];
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
     };
 
     extraLocaleSettings = {
