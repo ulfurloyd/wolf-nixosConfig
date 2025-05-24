c7dce6dad49b498af1447b1c4dd99fb4b3d9dd56
eklavya <eklavyasood@gmail.com>
2025-05-24 20:25:17 +0530

    style: 🎨 lykaon hyprpanel stuff ported to nyx

--- home/nyx/utils/hyprpanel/default.nix
+++ home/nyx/utils/hyprpanel/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ userSettings, inputs, ... }:
 {
   imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
 
@@ -22,7 +22,7 @@
       layout = {
         "bar.layouts" = {
           "*" = {
-            left = [ "dashboard" "workspaces" "windowtitle" ];
+            left = [ "dashboard" "cava" "workspaces" "windowtitle" ];
             middle = [ "media" ];
             right = [ "systray" "volume" "clock" "notifications" ];
           };
@@ -45,88 +45,15 @@
       theme = {
         bar.transparent = true;
         font.size = "14px";
+        matugen = true;
+        name = "monochrome";
+      };
+
+      wallpaper = {
+        enable = true;
+        image = "/home/wolf/.dotfiles/themes/${userSettings.nyxTheme}/wallpaper.png";
+        pywal = true;
       };
     };
   };
 }
-
-# # *.nix
-# { inputs, ... }:
-# {
-#   imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
-#
-#   programs.hyprpanel = {
-#
-#     # Enable the module.
-#     # Default: false
-#     enable = true;
-#
-#     # Automatically restart HyprPanel with systemd.
-#     # Useful when updating your config so that you
-#     # don't need to manually restart it.
-#     # Default: false
-#     systemd.enable = true;
-#
-#     # Add '/nix/store/.../hyprpanel' to your
-#     # Hyprland config 'exec-once'.
-#     # Default: false
-#     hyprland.enable = true;
-#
-#     # Fix the overwrite issue with HyprPanel.
-#     # See below for more information.
-#     # Default: false
-#     overwrite.enable = true;
-#
-#     # Import a theme from './themes/*.json'.
-#     # Default: ""
-#     theme = "gruvbox_split";
-#
-#     # Override the final config with an arbitrary set.
-#     # Useful for overriding colors in your selected theme.
-#     # Default: {}
-#     override = {
-#       theme.bar.menus.text = "#123ABC";
-#     };
-#
-#     # Configure bar layouts for monitors.
-#     # See 'https://hyprpanel.com/configuration/panel.html'.
-#     # Default: null
-#     layout = {
-#       "bar.layouts" = {
-#         "0" = {
-#           left = [ "dashboard" "workspaces" ];
-#           middle = [ "media" ];
-#           right = [ "volume" "systray" "notifications" ];
-#         };
-#       };
-#     };
-#
-#     # Configure and theme almost all options from the GUI.
-#     # Options that require '{}' or '[]' are not yet implemented,
-#     # except for the layout above.
-#     # See 'https://hyprpanel.com/configuration/settings.html'.
-#     # Default: <same as gui>
-#     settings = {
-#       bar.launcher.autoDetectIcon = true;
-#       bar.workspaces.show_icons = true;
-#
-#       menus.clock = {
-#         time = {
-#           military = true;
-#           hideSeconds = true;
-#         };
-#         weather.unit = "metric";
-#       };
-#
-#       menus.dashboard.directories.enabled = false;
-#       menus.dashboard.stats.enable_gpu = true;
-#
-#       theme.bar.transparent = true;
-#
-#       theme.font = {
-#         name = "CaskaydiaCove NF";
-#         size = "16px";
-#       };
-#     };
-#   };
-# }
